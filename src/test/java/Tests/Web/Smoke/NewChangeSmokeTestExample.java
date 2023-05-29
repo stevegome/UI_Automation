caac1c1d4e95b2986ae041ca2ab7afe23950e1d4
Stevan Gomes <stevan.gomes@wartsila.com>
2023-05-29 09:52:03 +0530

    modified new changes for framework

---
+++ src/test/java/Tests/Web/Smoke/NewChangeSmokeTestExample.java
@@ -0,0 +1,8 @@
+package Tests.Web.Smoke;
+
+public class NewChangeSmokeTestExample {
+    public static void main (String [] args)
+    {
+        System.out.println(" New Change Hello World");
+    }
+}
